6de5ed654659c83132b81302d7b0eb98fdc43b4d
Severin Spoerri <severin.spoerri@outlook.com>
2024-12-11 22:45:12 +0100

    add eslint and a layer created with nuxi init -t layer

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint"],
+  extends: ["../ui"],
+});
