e99ffb079aa89631c65d1dfa5efcfa74482e6183
Severin Spoerri <severin.spoerri@outlook.com>
2024-12-11 22:03:01 +0100

    add ESLint configuration and integrate checkbox component in app.vue

--- ui/app.vue
+++ ui/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <div>
+      <Checkbox></Checkbox>
+      <label>Checkbox</label>
+    </div>
     <NuxtWelcome />
   </div>
 </template>
--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint"],
 
   css: ["~/assets/css/styles.css"],
 });
