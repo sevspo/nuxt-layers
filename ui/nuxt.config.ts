345f3f87eeeb5b86f28656b16016b8e5af25e5c0
Severin Spoerri <severin.spoerri@outlook.com>
2024-12-14 22:34:19 +0100

    add new dependencies and update nuxt configuration for component paths

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,8 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint"],
 
-  css: ["~/assets/css/styles.css"],
+  alias: {
+    "~": currentDir,
+  },
+  css: [join(currentDir, "./assets/css/styles.css")],
+  components: [
+    {
+      path: join(currentDir, "./components"),
+      // this is required else Nuxt will autoImport `.ts` file
+      extensions: [".vue"],
+    },
+    {
+      path: join(currentDir, "./components/ui"),
+      // this is required else Nuxt will autoImport `.ts` file
+      extensions: [".vue"],
+    },
+  ],
 });
